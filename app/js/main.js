4a26698dce73832497abfa090b083777f820a79f
serjkovalko <serjkovalko@gmail.com>
2021-04-20 15:53:20 +0300

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
 
-})
+  $('.menu-nav__item-account').on('click', function () {
+    $('.form-box').toggleClass('form-box--active')
+  });
+
+  $('.menu-categories__item-first').on('click', function () {
+    $('.sub-menu-categories').toggleClass('sub-menu-categories--active')
+  });
+
+  $('.menu-nav__item-login').on('click', function () {
+    $('.form-box').toggleClass('form-box--active')
+  });
+
+});
+
